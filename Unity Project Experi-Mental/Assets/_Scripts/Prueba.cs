263afd2280379e2d009f00c0d57d515c33477081
Andres Camilo Urrego Restrepo <acur97.tk@gmail.com>
2020-03-02 08:22:43 -0500

    Ultima base

---
+++ Unity Project Experi-Mental/Assets/_Scripts/Prueba.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prueba : MonoBehaviour
+{
+    public Transform camara;
+    private bool agarrado;
+    private Transform original;
+
+    private void Awake()
+    {
+        original = transform.parent;
+    }
+
+    public void DejarseMover()
+    {
+        if (agarrado)
+        {
+            agarrado = false;
+            transform.parent = original;
+        }
+        else
+        {
+            agarrado = true;
+            transform.parent = camara;
+        }
+    }
+}
