46a17ff32c1349f04d075ffb6281c19ea392eeda
Andres Camilo Urrego Restrepo <acur97.tk@gmail.com>
2020-04-26 19:46:18 -0500

    no message

---
+++ Unity Project Experi-Mental/Assets/_Scripts/PlaneM.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneM : MonoBehaviour
+{
+    public Vector2 offset;
+
+    private Vector2 offset2;
+    private Material mat;
+
+    private readonly int mainT = Shader.PropertyToID("_BaseMap");
+
+    private void Awake()
+    {
+        mat = GetComponent<MeshRenderer>().material;
+    }
+
+    private void Update()
+    {
+        if (offset2.x > 500)
+        {
+            offset2.x = 0;
+        }
+        if (offset2.y > 500)
+        {
+            offset2.y = 0;
+        }
+        offset2 += new Vector2(0.1f * offset.x, 0.1f * offset.y);
+        mat.SetTextureOffset(mainT, offset2);
+    }
+}
